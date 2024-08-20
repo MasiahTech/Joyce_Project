0bc8bf42ffacf19ec944d7d1d712ba8eaffbd13e
MasiahTech <161542063+MasiahTech@users.noreply.github.com>
2024-08-20 15:17:39 -0400

    nodemailer

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 3000),
-  url: 'https://jadmin.onlycraft.cloud',
+  url: env('PUBLIC_URL','https://jadmin.onlycraft.cloud'),
   app: {
     keys: env.array('APP_KEYS'),
   },
